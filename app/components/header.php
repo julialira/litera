01cb9204cc367750d53492eeb1d8c7f357a753e0
Julia Lira <36300751+julialira@users.noreply.github.com>
2018-11-29 20:50:01 -0300

    Create header.php

---
+++ app/components/header.php
@@ -0,0 +1,44 @@
+<header class="">
+    <nav class="navbar navbar-expand-lg navbar-light bg-blue" style="box-shadow: 0px 2px 5px #1C1E1C; ">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse text-uppercase" id="navbarSupportedContent" >
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="index.php" style="color: #fff">início</a>
+          </li>
+          <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color: #fff">
+                Escritores
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="index.php#escritores">brígido ibanhes</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="index.php#escritores">emmanuel marinho</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="index.php#escritores">ílson osório</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="index.php#escritores">luciano serafim</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="index.php#escritores">márcia medeiros</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="index.php#escritores">marcos coelho</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="index.php#escritores">pedro larrê</a>
+              </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="index.php#videos"  style="color: #fff">fotos & Videos</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="index.php#somos"  style="color: #fff">QUEM SOMOS?</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="index.php#contato"  style="color: #fff">contato</a>
+          </li>
+ 
+        </ul>
+      </div>
+    </nav>
+  </header>
