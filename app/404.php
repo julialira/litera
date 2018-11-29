ef4584547a8309f10589736c88490d93f474da2e
Julia Lira <36300751+julialira@users.noreply.github.com>
2018-11-29 20:47:46 -0300

    Create 404.php

---
+++ app/404.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Nome do projeto</title>
+  <link rel="icon" href="">
+  <link rel="stylesheet" type="text/css" href="">
+  <link rel="stylesheet" type="text/css" href="">
+  <link href="" rel="stylesheet">
+  <link rel="stylesheet" href="" integrity="" crossorigin="anonymous"> 
+
+  <meta name="description" content=""/>
+    <meta name="keywords" content=", Index"/>
+    <meta name="robots" content="index, follow" />
+    
+    <!-- FACEBOOK -->
+    <meta property="og:title" content="" />
+    <meta property="og:site_name" content="" />
+    <meta property="og:url" content="" />
+    <meta property="og:image" content="" />
+    <meta property="og:image:type" content="image/png"/>
+    <meta property="og:image:width" content="" />
+    <meta property="og:image:height" content="" />
+    <meta property="og:description" content="" />
+    <meta property="og:locale" content="pt_BR" />
+    
+    <!-- TWITTER -->
+    <meta name="twitter:card" value="summary">
+    <meta name="twitter:site" content="">
+    <meta name="twitter:title" content="">
+    <meta name="twitter:description" content="">
+    <meta name="twitter:creator" content="">
+    <meta name="twitter:image" content="">
+    
+    <!-- Google+ -->
+    <meta itemprop="name" content="">
+    <meta itemprop="description" content="">
+    <meta itemprop="image" content="">
+    <meta itemprop="url" content="">
+    
+    <link rel="canonical" href="">
+</head>
+<body>
+  
+</body>
+</html>
