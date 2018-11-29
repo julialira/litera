5d87f70be5cd6d2a73e5349bb8d6adb7bf2f6e2f
Julia Lira <36300751+julialira@users.noreply.github.com>
2018-11-29 20:39:41 -0300

    Create header.php

---
+++ admin/components/header.php
@@ -0,0 +1,17 @@
+<nav class="navbar navbar-expand-lg navbar-light navbar-admin">
+  <a class="navbar-brand" href="admin.php"><img class="img-logo" src="../require/img/uni.png"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse navbar-content-item" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item"><a class="nav-link" href="admin.php">Inicial</a></li>
+      <!-- <li class="nav-item"><a class="nav-link" href="slides.php">Slides</a> -->
+    </ul>
+    <a target="_blank" href="../app/index.php" class="btn"><b>Ir para site</b></a>
+    <a href="actions/logout.php">Sair</a>
+  </div>
+
+
+</nav>
