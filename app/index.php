d718dfdefe7dbc29648314552f8ac2aa5db663cf
Julia Lira <36300751+julialira@users.noreply.github.com>
2018-12-01 08:05:00 -0300

    Update index.php

--- app/index.php
+++ app/index.php
@@ -5,42 +5,6 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>Literatura Favorável</title>
-  <link rel="icon" href="">
-  <link rel="stylesheet" type="text/css" href="">
-  <link rel="stylesheet" type="text/css" href="">
-  <link href="" rel="stylesheet">
-  <link rel="stylesheet" href="" integrity="" crossorigin="anonymous"> 
-
-  <meta name="description" content=""/>
-    <meta name="keywords" content=", Index"/>
-    <meta name="robots" content="index, follow" />
-    
-    <!-- FACEBOOK -->
-    <meta property="og:title" content="" />
-    <meta property="og:site_name" content="" />
-    <meta property="og:url" content="" />
-    <meta property="og:image" content="" />
-    <meta property="og:image:type" content="image/png"/>
-    <meta property="og:image:width" content="" />
-    <meta property="og:image:height" content="" />
-    <meta property="og:description" content="" />
-    <meta property="og:locale" content="pt_BR" />
-    
-    <!-- TWITTER -->
-    <meta name="twitter:card" value="summary">
-    <meta name="twitter:site" content="">
-    <meta name="twitter:title" content="">
-    <meta name="twitter:description" content="">
-    <meta name="twitter:creator" content="">
-    <meta name="twitter:image" content="">
-    
-    <!-- Google+ -->
-    <meta itemprop="name" content="">
-    <meta itemprop="description" content="">
-    <meta itemprop="image" content="">
-    <meta itemprop="url" content="">
-    
-    <link rel="canonical" href="">
 </head>
 <body>
   <!doctype html>
