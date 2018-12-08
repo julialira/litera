b8e40f98d9a7283ce6afdeea45e82bbb5bd17aa1
Julia Lira <36300751+julialira@users.noreply.github.com>
2018-12-08 18:21:06 -0300

    Update obras.php

--- app/obras.php
+++ app/obras.php
@@ -14,16 +14,219 @@
 
    <!-- ________COMECO HEADER_________-->
     <?php require_once "../app/components/header.php"; ?>
+    <div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-5">Sugestões de Leituras</h1>
+    <p class="lead">Aqui nós disponibilizamos para você algumas breves descrições de obras, e alguns poemas dos escritores que entrevistamos. Procure mais sobre os escritores e a literatura de Mato Grosso do Sul, vale muito a pena!</p>
+  </div>
+</div>
   <!-- FIM HEADER-->
 
   <!-- ________INICIO MAIN_________-->
-  <main class="">
-    
+  <main class="container pt-4">
+    <div class="row pr-2 pl-2">
+    <div class="card" style="">
+      <div class="card-body my-container">
+          <h5 class="card-title">Márcia Medeiros</h5>
+          <h6 class="card-subtitle mb-2 text-muted">Idade Média Narrada por um vampiro</h6>
+          <p class="card-text text-justify">Boa noite caros senhores, boa noite caras senhoras. Permitam-me que eu me apresente. Chamo-me Demétrius e sou tão velho quanto me lembro de ser. Demétrius somente. Esqueci o resto do meu nome nas areias de um tempo muito antigo. Eu vi o Império Romano arrojar-se em sua queda e vi a sua destruição. Vi o grande imperador Carlos Magno tornar-se um senhor coroado por um manto de estrelas. Querem saber mais sobre mim? Ouçam a minha história! Eu irei lhes contar a verdadeira história da Idade Média… Coisas que só um vampiro poderia saber.</p>
+          <a href="index.php#escritores" class="card-link">Márcia Medeiros</a>
+
+      </div>
+    </div> 
+    </div>
+
+    <!--_____FIM IDADE MEDIA NARRADA POR  UM VAMPIRO___-->
+
+    <div class="row pr-2 pl-2 pt-2">
+    <div class="card" style="">
+      <div class="card-body my-container">
+          <h5 class="card-title">Brígido Ibanhes</h5>
+          <h6 class="card-subtitle mb-2 text-muted">Silvino Jacques - O Ultimo Dos Bandoleiros </h6>
+          <p class="card-text text-justify">Esta obra relata a história de Silvino Jacques, gaúcho de São Borja (RS), churrasqueador e mulherengo, afilhado do Presidente Getúlio Vargas. No antigo Mato Grosso, na década de 1930, ele foi capitão revolucionário, e depois se tornou bandoleiro. Bom de pontaria com seu revólver e exímio cavaleiro, teve a missão de perseguir os paraguaios. Matou um paraguaio, membro da família poderosa dos 'dos Santos', que lhe moveu ferrenha perseguição durante anos. O delegado de captura Orcírio dos Santos, pai do ex-governador Zeca do PT, após muitos atropelos, consegue matar o bandoleiro, que morre nos braços da sua amada Raída.</p>
+          <a href="index.php#escritores" class="card-link">Brígido Ibanhes</a>
+
+      </div>
+    </div> 
+    </div>
+
+    <!--_____FIM SILVINO JACQUES___-->
+
+    <div class="row pr-2 pl-2 pt-2">
+     <div class="card" style="">
+      <div class="card-body my-container">
+          <h5 class="card-title">Emmanuel Marinho</h5>
+          <h6 class="card-subtitle mb-2 text-muted">Margens de Papel</h6>
+          <p class="card-text text-justify">Margens de Papel é a obra-símbolo do poeta Emmanuel. Neste Livro, seus poemas são como aqueles escritos que se encontram em margens de papel: não  tiveram lugar no corpo do texto, foram suprimidos, calados, mas não deixaram de ser ditos. Seus emblemáticos poemas vararam os anos ecoando nas inúmeras perfórmances e espetáculos do Poeta, muitos deles tornados ícones da poesia emmanuelina, como Índia velha e Genocíndio</p>
+          <a href="index.php#escritores" class="card-link">Emmanuel Marinho</a>
+
+      </div>
+    </div> 
+    </div>
+
+    <!--_____EMMANUEL MARINHO FIM___-->
+
+    <div class="row pr-2 pl-2 pt-2">
+    <div class="card" style="">
+      <div class="card-body my-container">
+          <h5 class="card-title">Luciano Serafim</h5>
+          <h6 class="card-subtitle mb-2 text-muted">Mordendo as Lábias</h6>
+          <p class="card-text text-justify">"Mordendo as Lábias" é uma obra bem contemporânea, com um tom de protesto, ora irônico, com uma temática atual, embora não deixe de tratar das eternas questões universais: as emoções. O livro é composto por pequenos poemas diagramados que ora são conexos, ora são rebeldes e desmétricos.</p>
+          <a href="index.php#escritores" class="card-link">Luciano Serafim</a>
+
+      </div>
+    </div> 
+    </div>
+
+     <!--_____FIM MORDENDO AS LÁBIAS___-->
+     <div class="row pr-2 pl-2 pt-2">
+    <div class="card" style="">
+      <div class="card-body my-container">
+          <h5 class="card-title">Marcos Coelho</h5>
+          <h6 class="card-subtitle mb-2 text-muted">Dourados e sua Natureza em Prosa, Poesia e Cordel</h6>
+          <p class="card-text text-justify">"Mordendo as Lábias" é uma obra bem contemporânea, com um tom de protesto, ora irônico, com uma temática atual, embora não deixe de tratar das eternas questões universais: as emoções. O livro é composto por pequenos poemas diagramados que ora são conexos, ora são rebeldes e desmétricos.</p>
+          <a href="index.php#escritores" class="card-link">Marcos Coelho</a>
+
+      </div>
+    </div> 
+    </div>
+
+     <!--_____Dourados e sua natureza em prosa, poesia e cordel___-->
+
+        <div class="row pr-2 pl-2 pt-2">
+    <div class="card" style="">
+      <div class="card-body my-container">
+          <h5 class="card-title">Ílson Osório</h5>
+          <h6 class="card-subtitle mb-2 text-muted">Minha Infância na Fazenda</h6>
+          <p class="card-text text-justify">Dourados realmente é uma terra diferente, se não houvesse nascido nesta cidade, certamente iria adotá-la como minha. Se de fora eu fosse, poderia ter sido atraído pelas suas terras férteis, seus campos planos e vastos, o doce das suas guaviras, altas ou rasteiras, que tanto atráiram os chegantes em outras épocas, ou, até mesmo pelo seu tereré, cantado e decantado, em prosas e versos, em muitos livros e escritos.</p>
+          <a href="index.php#escritores" class="card-link">Ílson Osório</a>
+
+      </div>
+    </div> 
+    </div>
+
+     <!--_____Minha infancia na fazenda___-->
+
+    <div class="row pr-2 pl-2 pt-2">
+    <div class="card" style="">
+      <div class="card-body my-container">
+          <h5 class="card-title">Pedro Larrê</h5>
+          <div class="card">
+              <div class="card-body"> 
+                <h6 class="card-subtitle mb-2 text-muted">Celas Sublimes</h6>
+              <p class="card-text text-justify">
+
+Eu fugia/Do compromisso de me assumir/De revelar o que eu sou/E declarar que sou ladrão/Eu fugia..//
+
+Corria contra o tempo de mim/
+Na sombra do meu próprio eu/
+Eu apagava o meu sol//
+
+Eu fugia.../
+Como o diabo foge da cruz/
+Como o vampiro corre da luz/
+Eu corria sem saber por quê//
+
+Até o dia em que fui pego/
+E por fim aprisionado/
+Eles diziam: - criminoso./
+Tu terás que ser julgado!//
+
+E se eu for o culpado/
+Tenho que pagar pelo meu crime/
+Passarei inúmeros dias da minha vida/
+Na cela da tristeza sublime//
+
+Talvez o que eu sinta seja errado/
+Posso ter perdido a razão/
+Essa é a era dos desgraçados/
+É proibido despertar amor no coração//
+
+Por isso fui aprisionado/
+Amarrado, cuspido e torturado/
+De longe sou o criminoso/
+E devo pagar pelo meu crime//
+
+Neste mundo de ódio/
+É proibido despertar amor no outro/
+Fui caçado, julgado e condenado/
+Por ter roubado o coração de um garoto.</p>
+              </div> 
+
+          </div>
+          <div class="card mt-2 mb-4">
+              <div class="card-body "> 
+                <h6 class="card-subtitle mb-2 text-muted">Equidade</h6>
+              <p class="card-text text-justify ">Quem sou eu/
+  Para julgar meus semelhantes/
+  Se também sou feito de carne/
+  E não sou pleno de sabedoria//
+  Quem sou eu/
+  Para condenar tais atitudes/
+  Para dizer tais vereditos/
+  Se sou um homem/
+  E homens jamais será Deus//
+  Eu acredito/
+  Na força do pensamento/
+  Na energia, espaço e tempo/
+  Que uma mente arquitetou//
+  Eu acredito/
+  Na vida e na felicidade/
+  Por um momento equidade//
+  O mundo necessita de paz/
+  E o ser humano precisa reaprender a amar//
+  É preciso ter peito para respeitar/
+  É preciso respeito na hora de amar/
+  É preciso aprender aceitar as diferenças/
+  Cada ser é único em seu jeito de ser/
+  Seria uma tragédia se todos fossem iguais//
+  Então por isso.../
+  Hoje estou a fim de tentar compreender/
+  Ao invés de ser compreendido/
+  Hoje estou afim de acolher/
+  Uma vez já fui acolhido/
+  Hoje estou afim de ajudar/
+  Com isso posso ser ajudado/
+  É amando hoje um pouco de cada um/
+  Que amanhã quem sabe/
+  Eu também possa ser amado/
+  E quem sabe um dia/
+  Eu possa me deitar//
+  Com a cabeça tranquila/
+  E na paz do nosso senhor/
+  Dizer pra mim mesmo:/
+  -Agora posso descansar!/
+  Pois confio na humanidade/
+  E tenho ciência/
+  De que cada homem e cada mulher/
+  Aprendeu a viver dignamente com o outro/
+  E por isso agora/
+  O mundo merece /
+  O melhor que o próprio mundo/
+  Possa lhes oferecer/
+  Em verdade vos digo/
+  Isso só depende/
+  De mim e de você.
+  </p>
+              </div> 
+               
+          </div>
+          
+          <a href="index.php#escritores" class="card-link">Pedro Larrê</a>
+
+      </div>
+    </div> 
+    </div>
+
+   
   </main>
     <!-- FIM MAIN-->
 
     <!-- ________SECAO FOOTER_________-->
-    <?php require_once "../app/components/footer.php"; ?>
+    <div class="pt-4">  
+       <?php require_once "../app/components/footer.php"; ?>
+    </div>
+   
 
  
     <!-- Optional JavaScript -->
