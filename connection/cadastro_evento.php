103fecf4b79b22ed83e5aadcdc3dc646d744d698
Julia Lira <36300751+julialira@users.noreply.github.com>
2018-12-08 18:29:12 -0300

    Create cadastro_evento.php

---
+++ connection/cadastro_evento.php
@@ -0,0 +1,24 @@
+<?php
+    include_once("../require/functions/files.php");
+    include_once("connection.php");
+    $name = $_POST['name'];
+    $event = $_POST['event'];
+    $file = $_FILES['file'];
+
+    $conn = connect();
+
+    define('DEST_DIR', 'C:\xampp\htdocs\litera\require\uploads\ ');
+
+   if(saveImage($name, "file", DEST_DIR)){
+    $query = "INSERT INTO arquivo(name, event, file, date) VALUES ('$name','$event', '$salvarArquivo', '2018-11-30')";
+
+    $resultado_usuario = mysqli_query($conn, $query);
+
+    header("Location: ../app/galeria.php");
+   }else{
+    echo "<h1>Erro</h1>";
+   }
+    
+    
+  
+?>
